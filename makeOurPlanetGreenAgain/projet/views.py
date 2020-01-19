d5a0d19e87820aea9fa8b050c12b2d4b8f0e2791
romane <romanepoirier0@gmail.com>
2020-01-19 18:49:55 +0100

    Add project page

--- makeOurPlanetGreenAgain/projet/views.py
+++ makeOurPlanetGreenAgain/projet/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Projet
+from .forms import ProjectForm
+import logging
+
+log = logging.getLogger(__name__)
 
 
 def index(request):
@@ -14,3 +18,17 @@ def detail(request, project_id):
     project = get_object_or_404(Projet, pk=project_id)
     context = {'project': project}
     return render(request, "projet/detail.html", context)
+
+
+def add(request):
+    if request.method == 'GET':
+        form = ProjectForm()
+    else:
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+        else:
+            log.error(form.errors)
+        return redirect('projet:index')
+
+    return render(request, "projet/add.html", {'form': form})
