68e32388b6d4b74a8d752cdff8752db9af9aedc6
romane <romanepoirier0@gmail.com>
2020-01-20 10:36:59 +0100

    Add search function

--- makeOurPlanetGreenAgain/plateforme/urls.py
+++ makeOurPlanetGreenAgain/plateforme/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('auth/', views.login_view, name='login_view'),
     path('logout/', views.logout_view, name='logout_view'),
     path('register/', views.register_view, name='register_view'),
-
+    path('search/', views.search, name='search')
 ]
