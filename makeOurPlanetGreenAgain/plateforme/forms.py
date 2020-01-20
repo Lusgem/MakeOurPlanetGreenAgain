457af51f2cce5bc2cf8b0a3ae3c490183444da61
romane <romanepoirier0@gmail.com>
2020-01-20 18:08:46 +0100

    Add comment zone

--- makeOurPlanetGreenAgain/plateforme/forms.py
+++ makeOurPlanetGreenAgain/plateforme/forms.py
@@ -1,21 +1,36 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Commentaire
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(label='username', max_length=100)
     password = forms.CharField(label='password', max_length=100)
 
+
 class RegisterForm(UserCreationForm):
     email = forms.EmailField()
     first_name = forms.CharField(label="first_name",max_length=20)
     last_name = forms.CharField(label="last_name",max_length=20)
+
     class Meta:
-        model= User
-        fields= ['username', 'email', 'password1', 'password2', 'first_name','last_name']
+        model = User
+        fields = ['username', 'email', 'password1', 'password2', 'first_name', 'last_name']
+
 
 class ContactForm(forms.Form):
     name = forms.CharField(required=True)
     mail = forms.EmailField(required=True)
     subject = forms.CharField(required=False)
     message = forms.CharField(required=True, widget=forms.Textarea)
+
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Commentaire
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea()
+        }
--- makeOurPlanetGreenAgain/plateforme/models.py
+++ makeOurPlanetGreenAgain/plateforme/models.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.models import User
 from django.db import models
 from projet.models import Projet
+from datetime import datetime
 
-# Create your models here.
+
 class Commentaire(models.Model):
     auto_increment_id = models.AutoField(primary_key=True)
     content = models.CharField(max_length=2500)
     projet = models.ForeignKey(Projet, on_delete=models.CASCADE)
     utilisateur = models.ForeignKey(User, on_delete=models.CASCADE)
+    publication_date = models.DateTimeField(default=datetime.now)
